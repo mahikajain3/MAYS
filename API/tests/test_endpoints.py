ba3e37e5f577ada0e8014460db2d667b1cd6998d
Mahika Jain <mj2249@nyu.edu>
2021-09-30 12:12:34 -0400

    create test file that fails

---
+++ API/tests/test_endpoints.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, skip
+
+import API.endpoints as ep
+
+
+class EndpointTestCase(TestCase):
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+
+	def yest_hello(self):
+		self.assertTrue(False)
