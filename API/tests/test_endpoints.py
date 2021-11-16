988b1c1deef0cbeaf9f7be09475b6d6872effabf
SarahGuu <xg737@nyu.edu>
2021-11-16 13:37:57 -0500

    test heroku DO NOT DEPLOY with failed testcase

--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -27,7 +27,7 @@ class EndpointTestCase(TestCase):
                 hello = ep.HelloWorld(Resource)
                 ret = hello.get()
                 self.assertIsInstance(ret, dict)
-                self.assertIn(ep.HELLO, ret)
+                self.assertNotIn(ep.HELLO, ret)
 
 	def test_create_user(self):
 		"""
