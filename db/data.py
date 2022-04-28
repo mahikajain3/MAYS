a4e3feeece7c481949e0ad30979b13da6434dcd4
Mahika Jain <mj2249@nyu.edu>
2022-04-28 14:47:34 -0400

    Cleaning whitespaces

--- db/data.py
+++ db/data.py
@@ -7,7 +7,7 @@ Gradually, we will fill in actual calls to our datastore.
 import os
 import json
 from bson import json_util
-import utils
+# import utils
 import db.db_connect as dbc
 
 MAYS_HOME = os.environ["MAYS_HOME"]
@@ -194,18 +194,20 @@ def add_user(netid, firstname, lastname, barcode=999999999):
 #     else:
 #         dbc.insert_doc(USERS, {USERS_NM: username, PASSWORD: password})
 
-def add_user2(netid,firstname, lastname, password, barcode=999999999,):
-    """
-    Add a new user to the user database.
-    """
-    if netid_exists(netid):
-        return DUPLICATE
-    else:
-        salt = utils.gen_salt()
-        hashed = utils.hash_pw(salt,password)
-        dbc.insert_doc(USERS, {NETID: netid, FIRST_NM: firstname,
-                       LAST_NM: lastname, BARCODE: barcode, PASSWORD: hashed})
-        # for now netid is all 0
+# def add_user2(netid,firstname, lastname, password, barcode=999999999):
+#     """
+#     Add a new user to the user database.
+#     """
+#     if netid_exists(netid):
+#         return DUPLICATE
+#     else:
+#         salt = utils.gen_salt()
+#         hashed = utils.hash_pw(salt,password)
+#         dbc.insert_doc(USERS, {NETID: netid, FIRST_NM: firstname,
+#                        LAST_NM: lastname, BARCODE: barcode,
+#                        PASSWORD: hashed})
+#         # for now netid is all 0
+
 
 def add_workshop(workshopname):
     """
